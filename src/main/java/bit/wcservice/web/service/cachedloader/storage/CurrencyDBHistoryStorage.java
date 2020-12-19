0019c87218477b274cdb8361872bf487171b58ce
Roman <roma_klimovitskiy@mail.ru>
2020-12-20 00:36:19 +0500

    Removed unnecessary import

--- src/main/java/bit/wcservice/web/service/cachedloader/storage/CurrencyDBHistoryStorage.java
+++ src/main/java/bit/wcservice/web/service/cachedloader/storage/CurrencyDBHistoryStorage.java
@@ -4,7 +4,6 @@ import bit.wcservice.database.entity.datarecord.Currency;
 import bit.wcservice.database.service.CurrencyService;
 import bit.wcservice.util.datarange.DateRange;
 import bit.wcservice.web.service.cachedloader.HistoryStorage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
